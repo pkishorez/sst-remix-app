6b111a966c67a07095fc800bcbc35c59dad1fa62
Kishore <kishore.iiitn@gmail.com>
2022-09-06 22:32:07 +0530

    feat: Add prettier. For debug add random.

--- web/app/routes/index.tsx
+++ web/app/routes/index.tsx
@@ -1,18 +1,21 @@
-import {json, LoaderFunction} from "@remix-run/node";
-import {useLoaderData} from "@remix-run/react";
+import { json, LoaderFunction } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
 
-export const loader: LoaderFunction = ()=>{
+export const loader: LoaderFunction = () => {
   return json({
-    url: process.env.ENDPOINT
-  })
-}
+    url: process.env.ENDPOINT,
+    random: Math.random() * 1000,
+  });
+};
 
 export default function Index() {
-  const {url} = useLoaderData();
+  const { url, random } = useLoaderData();
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
       <h1>Welcome to Remix</h1>
-      <h2>Url is {url}</h2>
+      <h2>
+        Url is {url} {random}
+      </h2>
     </div>
   );
 }
